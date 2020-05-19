c8c4363bc49f0d94c89cdf095f609377990967d2
Fabio Gadau <fabiogadau92@gmail.com>
2020-05-19 14:28:59 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>PHP Google FAQ</title>
+   <!-- Stylesheet -->
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
+   <link rel="stylesheet" href="assets/styles/main.css">
+</head>
+<body>
+   
+   <?php
+      /*
+      *  Riscrivere questa pagina del sito google https://policies.google.com/faq.
+      *  Ci sono diverse domande con relative risposte. Gestire il “Database” (array) e la visualizzazione di queste domande e risposte con PHP.
+      */
+      
+   ?>
+
+</body>
+</html>
